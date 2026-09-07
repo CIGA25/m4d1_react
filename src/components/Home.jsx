7f6a6a50d4b62748396db005e6296c918aed9bb0
Carlos Guasch Abraham <cguasch20@gmail.com>
2026-09-06 21:02:39 -0300

    se corrigen rutas de imagenes para vite+react

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,5 +1,8 @@
 import CardPizza from "./CardPizza"
 import Header from "./Header"
+import napo from "../assets/img/napo.webp"
+import jardin from "../assets/img/jardin.webp"
+import medite from "../assets/img/medite.webp"
 
 function Home() {
     return (
@@ -10,19 +13,19 @@ function Home() {
                 name="Napolitana"
                 price={10990}
                 receta="Salsa de tomate, mozzarella, tomate fresco, albahaca y aceite de oliva."
-                url="./src/assets/img/napo.webp"
+                url={napo}
                 />
                 <CardPizza
                 name="Jardinera"
                 price={11990}
                 receta="Salsa de tomate, mozzarella, champiñones, pimentón, cebolla, aceitunas y choclo."
-                url="./src/assets/img/jardin.webp"
+                url={jardin}
                 />
                 <CardPizza
                 name="Mediterranea"
                 price={12990}
                 receta="Salsa de tomate, mozzarella, tomates cherry, aceitunas negras, queso feta, rúcula y aceite de oliva."
-                url= "./src/assets/img/medite.webp"
+                url={medite}
                 />
             </section>
         </main>
