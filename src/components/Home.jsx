a4e605de4f7ca41d209bac2f7a4a085312d6f2ed
Carlos Guasch Abraham <cguasch20@gmail.com>
2026-09-06 21:09:05 -0300

    Fix: corrección de rutas de imagen

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,8 +1,8 @@
 import CardPizza from "./CardPizza"
 import Header from "./Header"
-import napo from "../assets/img/napo.webp"
-import jardin from "../assets/img/jardin.webp"
-import medite from "../assets/img/medite.webp"
+import Napo from "../assets/img/napo.webp"
+import Jardin from "../assets/img/jardin.webp"
+import Medite from "../assets/img/medite.webp"
 
 function Home() {
     return (
@@ -13,19 +13,19 @@ function Home() {
                 name="Napolitana"
                 price={10990}
                 receta="Salsa de tomate, mozzarella, tomate fresco, albahaca y aceite de oliva."
-                url={napo}
+                url={Napo}
                 />
                 <CardPizza
                 name="Jardinera"
                 price={11990}
                 receta="Salsa de tomate, mozzarella, champiñones, pimentón, cebolla, aceitunas y choclo."
-                url={jardin}
+                url={Jardin}
                 />
                 <CardPizza
                 name="Mediterranea"
                 price={12990}
                 receta="Salsa de tomate, mozzarella, tomates cherry, aceitunas negras, queso feta, rúcula y aceite de oliva."
-                url={medite}
+                url={Medite}
                 />
             </section>
         </main>
